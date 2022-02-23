a0a00b8e87f6c3538f1b60853f0847539f9ab105
xassasinsoulx <erdemcalikoglu@windowslive.com>
2022-02-23 12:54:15 -0500

    Main menu buttons activated, design changes, gameselection page initiated

--- guess it/app/src/main/java/com/example/guessit/GameSelection.java
+++ guess it/app/src/main/java/com/example/guessit/GameSelection.java
@@ -3,15 +3,12 @@ package com.example.guessit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-
-public class Leaderboard extends AppCompatActivity {
+public class GameSelection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leaderboard);
+        setContentView(R.layout.activity_game_selection);
     }
 }
--- guess it/app/src/main/java/com/example/guessit/MainActivity.java
+++ guess it/app/src/main/java/com/example/guessit/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.guessit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+    }
+
+    public void leaderboard(View view){
+        Intent leaderboardIntent = new Intent(getApplicationContext(), Leaderboard.class);
+        startActivity(leaderboardIntent);
+    }
+
+    public void gameInitialize(View view){
+        Intent gameIntent = new Intent(getApplicationContext(), GameSelection.class);
+        startActivity(gameIntent);
     }
 }
