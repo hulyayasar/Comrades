863a9863435b2d4ee2b850743a756827b2668c16
xassasinsoulx <erdemcalikoglu@windowslive.com>
2022-03-24 01:26:48 -0400

    fixed minor problems

--- guess it/app/src/main/java/com/example/guessit/GameSelection.java
+++ guess it/app/src/main/java/com/example/guessit/GameSelection.java
@@ -82,38 +82,17 @@ public class GameSelection extends AppCompatActivity implements AdapterView.OnIt
 
         String key = ref.push().getKey();
         ref.child(key).child("username").setValue(name_1);
-
-        refLb.child(key).child(name_1).setValue(0);
+        refLb.child(name_1).setValue(0);
 
         String key1 = ref.push().getKey();
         ref.child(key1).child("username").setValue(name_2);
         refLb.child(name_2).setValue(0);
 
-        refLb.child(key).child("username").setValue(name_1);
-        refLb.child(key).child("points").setValue(0);
-
-        String key1 = ref.push().getKey();
-        ref.child(key1).child("username").setValue(name_2);
-        refLb.child(key1).child("username").setValue(name_2);
-        refLb.child(key1).child("points").setValue(0);
-
         refSettings.child("difficulty").setValue(user_difficulty);
         refSettings.child("selection").setValue(user_choise);
 
         Intent gameStart = new Intent(getApplicationContext(), Game.class);
-
-
-        gameStart.putExtra("name1", name_1);
-        gameStart.putExtra("name2", name_2);
-        gameStart.putExtra("player1ID", key);
-        gameStart.putExtra("player2ID", key1);
-
-        refSettings.child("difficulty").setValue(user_difficulty);
-        refSettings.child("selection").setValue(user_choise);
-
-        Intent gameStart = new Intent(getApplicationContext(), Game.class);
-
-
+        
         gameStart.putExtra("name1", name_1);
         gameStart.putExtra("name2", name_2);
         gameStart.putExtra("player1ID", key);
